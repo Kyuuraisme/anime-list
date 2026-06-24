f3f139df4b416ae0951be786fd54485361bb2498
Ee <153446470+egxneroo@users.noreply.github.com>
2026-06-24 10:26:01 +0700

    menamba fitur login dengan database

--- models/m_akun.php
+++ models/m_akun.php
@@ -1,39 +1,47 @@
 <?php
-class m_akun {
+class m_akun
+{
     private $conn;
 
-    public function __construct($conn) {
+    public function __construct($conn)
+    {
         $this->conn = $conn;
     }
 
     // Fungsi login
-    public function login($request) {
-    $nama_user = $request['nama_user'];
-    $password  = $request['password'];
+    public function login($request)
+    {
+        $nama_user = $request['nama_user'];
+        $password  = $request['password'];
 
-    $query = "SELECT * FROM master.m_akun WHERE nama_user = $1";
-    $result = pg_query_params($this->conn, $query, [$nama_user]);
-    $row = pg_fetch_assoc($result);
+        $query = "SELECT * FROM master.m_akun WHERE nama_user = $1";
+        $result = pg_query_params($this->conn, $query, [$nama_user]);
+        $row = pg_fetch_assoc($result);
 
-    if ($row && password_verify($password, $row['password'])) {
-        return $row; // login berhasil
+        if ($row !== false && password_verify($password, $row['password'])) {
+            return $row; // login berhasil
+        }
+        return false; // login gagal
     }
-    return false; // login gagal
-}
+
+
 
 
     // Fungsi register
     public function addAkun($request) {
-        $nama_user = $request['nama_user'];
-        $no_ponsel = $request['no_ponsel'];
-        $gender_user = $request['gender_user'];
-        $password = $request['password'];
+    $nama_user   = $request['nama_user'];
+    $no_ponsel   = $request['no_ponsel'];
+    $gender_user = $request['gender_user'];
+    $password    = $request['password'];
 
-        $query = "INSERT INTO master.m_akun (nama_user, no_ponsel, gender_user, password, created_at)
-                  VALUES ($1, $2, $3, $4, now())";
-        $result = pg_query_params($this->conn, $query, [$nama_user, $no_ponsel, $gender_user, $password]);
+    // bikin hash dari password
+    $hash = password_hash($password, PASSWORD_DEFAULT);
 
-        return $result;
-    }
+    $query = "INSERT INTO master.m_akun (nama_user, no_ponsel, gender_user, password, created_at)
+              VALUES ($1, $2, $3, $4, now())";
+    $result = pg_query_params($this->conn, $query, [$nama_user, $no_ponsel, $gender_user, $hash]);
+
+    return $result;
 }
-?>
+
+}
